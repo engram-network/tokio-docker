6f9120f84f201a9913ba67a1e975ef7fd38a04ab
wulantheforstone <joywibowo10@gmail.com>
2023-11-21 08:34:08 +0700

    change image docker staking-cli

--- scripts/validator-build.sh
+++ scripts/validator-build.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v $(pwd)/validator_keys:/app/validator_keys engramnet/staking-deposit-cli:v1 existing-mnemonic --num_validators=1 --validator_start_index=0
+docker run -it --rm -v $(pwd)/validator_keys:/app/validator_keys registry.digitalocean.com/cri-private/engramnet/staking-deposit-cli:v2 existing-mnemonic --num_validators=1 --validator_start_index=0
